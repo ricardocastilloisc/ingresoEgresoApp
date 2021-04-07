0ea771d340b4020a16599918ab5f64c636632ded
Ricardo Orlando Castillo Olivera <ricardocastilloisc@gmail.com>
2021-04-06 21:34:03 -0500

    Verificar si existe la suscripcion para no marcar error
    acuerdate que los states en el retorno no es const sino let ya que solo parche sin fijarme que solo el return muta

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -37,7 +37,10 @@ export class AuthService {
             this.store.dispatch(authActions.setUser({ user }));
           });
       } else {
-        this.userSubscription.unsubscribe();
+        if(this.userSubscription)
+        {
+          this.userSubscription.unsubscribe();
+        }
         this.store.dispatch(authActions.unSetUser());
       }
     });
