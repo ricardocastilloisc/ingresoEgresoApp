bf32b4c92720507fa51f299a445ad99d80a10c47
Ricardo Orlando Castillo Olivera <ricardocastilloisc@gmail.com>
2021-04-07 09:05:47 -0500

    Pantalla para capturar ingresos y egresos

--- src/app/ingreso-egreso/ingreso-egreso.component.ts
+++ src/app/ingreso-egreso/ingreso-egreso.component.ts
@@ -1,4 +1,6 @@
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-ingreso-egreso',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IngresoEgresoComponent implements OnInit {
 
-  constructor() { }
+  ingresoForm: FormGroup;
+  tipo: string = 'ingreso';
+
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
+    this.ingresoForm = this.fb.group(
+      {
+        descripcion: ['', Validators.required],
+        monto: ['', Validators.required]
+      });
   }
 
+  guardar = () =>
+  {
+    if(this.ingresoForm.invalid){return}
+    console.log(this.ingresoForm.value)
+    console.log(this.tipo)
+  }
 }
