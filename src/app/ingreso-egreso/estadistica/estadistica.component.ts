b905ed96d689f01e60966a318b8c3736c25d3f45
Ricardo Orlando Castillo Olivera <ricardocastilloisc@gmail.com>
2021-04-07 19:06:34 -0500

    Estadística básica

--- src/app/ingreso-egreso/estadistica/estadistica.component.ts
+++ src/app/ingreso-egreso/estadistica/estadistica.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.reducer';
+import { Subscription } from 'rxjs';
+import { IngresoEgreso } from '../../models/ingreso-egreso.model';
 
 @Component({
   selector: 'app-estadistica',
   templateUrl: './estadistica.component.html',
-  styles: []
+  styles: [],
 })
-export class EstadisticaComponent implements OnInit {
+export class EstadisticaComponent implements OnInit, OnDestroy {
+  ingresos: number = 0;
+  egresos: number = 0;
 
-  constructor() { }
-
-  ngOnInit() {
+  TotalEgresos: number = 0;
+  TotalIngresos: number = 0;
+  ingresosEgresosSub: Subscription;
+  constructor(private store: Store<AppState>) {}
+  ngOnDestroy(): void {
+    this.ingresosEgresosSub.unsubscribe();
   }
 
+  ngOnInit() {
+    this.ingresosEgresosSub = this.store
+      .select('ingresosEgresos')
+      .subscribe(({items}) => {
+        this.generarEstadistica(items)
+      });
+  }
+
+
+  generarEstadistica = (items:IngresoEgreso[]) =>
+  {
+    this.TotalEgresos= 0;
+    this.TotalIngresos = 0;
+    this.ingresos = 0;
+    this.egresos = 0;
+    items.forEach(e =>
+      {
+        if(e.tipo ==='ingreso')
+        {
+          this.TotalIngresos += e.monto;
+          this.ingresos++;
+        }else
+        {
+          this.TotalEgresos += e.monto;
+          this.egresos++;
+        }
+      })
+  }
 }
