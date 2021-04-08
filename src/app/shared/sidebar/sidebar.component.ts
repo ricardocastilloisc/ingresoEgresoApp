7252812c2ffeb150dc886a249cf70b8866cbea42
Ricardo Orlando Castillo Olivera <ricardocastilloisc@gmail.com>
2021-04-07 19:47:49 -0500

    Cambio de nombre

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -2,6 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { AuthService } from '../../services/auth.service';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.reducer';
+import { Observable } from 'rxjs';
+import { Usuario } from 'src/app/models/usuario.model';
+import { map } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-sidebar',
@@ -10,10 +16,14 @@ import { AuthService } from '../../services/auth.service';
 })
 export class SidebarComponent implements OnInit {
 
+
+  userObs$:Observable<Usuario>
   constructor( private authService: AuthService,
-               private router: Router) { }
+               private router: Router,
+               private store: Store<AppState>) { }
 
   ngOnInit() {
+    this.userObs$ = this.store.select('user').pipe(map(({user}) => user))
   }
 
   logout() {
